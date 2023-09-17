5f596d7b2fe7a5cf8ade2b64ebd60f26a641f636
Ruthvik.Kamarasu <ruthvik.kamarasu@gupshup.io>
2023-09-17 02:28:02 -0700

    created man of mass bash script

---
+++ who_is_the_mass_bash/saikiran.sh
@@ -0,0 +1,38 @@
+
+mass_loop(){
+counter=1
+while [ $counter -le 5 ]
+do
+SLEEP 0.5
+echo MASSS!
+((counter++))
+done
+SLEEP 1
+}
+
+type_writer_text(){
+
+message=$1 
+	for ((i=0; i<${#message}; i++)); 
+	do 
+	echo "after 100" | tclsh; 
+	printf "${message:$i:1}"; 
+done; 
+echo;
+}
+
+mass_loop
+
+type_writer_text "Who is man of mass?"
+
+read varname
+if [ "$varname" == 'sai kiran' ]
+then
+	type_writer_text "GOOD, YOU LIVE ANOTHER DAY"
+else
+	type_writer_text "YOU FORGOT THE MAN OF MASS!!!! , YOU WILL BE HACKED!"
+	SLEEP 2
+	ascii-image-converter ~/Downloads/saikiran-is-man-of-mass.jpeg -C -b --dither
+	mass_loop
+	type_writer_text "SAI KIRAN IS THE MAN OF MAAASSSSSSS"
+fi
